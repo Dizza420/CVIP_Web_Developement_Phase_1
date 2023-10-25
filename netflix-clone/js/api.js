c081cef78af3f4f2a776a49a060d27c2fabaa6e4
Adrija-sarkar <92675081+Dizza420@users.noreply.github.com>
2023-10-25 11:50:28 +0530

    Add files via upload

---
+++ netflix-clone/js/api.js
@@ -0,0 +1,5 @@
+const api_key='435611b4da39d57c8e9c7a511d9b28c1';
+
+let img_url = "https://image.tmdb.org/t/p/w500/";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
